d8cf41df9762b8091f4008fb04910d79fc8d2afb
infeeeee <gyetpet@mailbox.org>
2020-01-19 23:31:25 +0100

    Added sound to vol skin

---
+++ @Resources/playSound.bat
@@ -0,0 +1,10 @@
+@echo off
+set file=C:\Windows\Media\Windows Background.wav
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
+start /min sound.vbs
